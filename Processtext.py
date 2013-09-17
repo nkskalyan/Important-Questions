51a423f103ef9507cab4087480802333576afa8e
Kalyanasundaram <nks.22.1992@gmail.com>
2013-09-17 19:41:52 +0530

    Perfectly working Portions in Dict format

--- Processtext.py
+++ Processtext.py
@@ -12,20 +12,27 @@ def WriteDictToFile(fileDict):
 			file.write("\n".join(content))
 		elif type(content)==None:
 			pass
-		elif type(content)==dict:
-			print content
+		elif type(content)==dict:			
+			file.write(str(content))
+			print len(content)
 		else:
 			file.write(content)
 		file.close()
 
 #In the PDf read, there is no space between unit name and first topic.
 #Replace the same using this function
-def SeparateWhiteSpace(str):
-	toReplace=re.findall("[A-Z][a-z]+[A-Z]",str)
+def SeparateWhiteSpace(str,toReplace=None):
+	noOfCharToReplace=1
+	if not toReplace:
+		toReplace=re.findall("[A-Z][a-z]+[A-Z]",str)
+	else:	#Physics paper
+		toReplace=re.findall("[A-Z][\s]*[A-Z]+[a-z]",str)
+		noOfCharToReplace = 2
+		#toReplace= [text[:len(text)] for text in toReplace ]
 	#print toReplace
 	for text in toReplace:
 		#Replacement for the word without space
-		replacedText = text[:len(text)-1]+"::::"+text[len(text)-1]
+		replacedText = text[:len(text)-noOfCharToReplace]+":::"+text[len(text)-noOfCharToReplace]
 		#print replacedText
 		str =  re.sub(text,replacedText,str)
 		
@@ -36,7 +43,7 @@ def SeparateWhiteSpace(str):
 def getPortionDictList(portionDict):
 	splitPortDict={}
 	for subject,portion in portionDict.iteritems():
-		port = re.split("UNIT [0-9]+:",portion)
+		port = re.split("UNIT[\s]+[0-9]+:",portion)
 		if port:
 			splitPortDict[subject+"_split"]=port
 		#Just for testing, To be removed
@@ -46,6 +53,7 @@ def getPortionDictList(portionDict):
 #Does basic removal of whitespaces and separation of portion for each subject
 def findPortion():
 	string = open(portionPath,"r").read()
+	string = re.sub(r'\([^\)]+\)',' ',string)
 	unformattedList = re.split("MATHEMATICS",string)
 	#Remove unwanted stuff before Math
 	unformattedList = re.split("PHYSICS",unformattedList[1])
@@ -56,7 +64,7 @@ def findPortion():
 	chemistry = unformattedList[1].split("SYLLABUS")[0]
 	portions={}
 	portions["math"]=SeparateWhiteSpace(math)
-	portions["physics"]=SeparateWhiteSpace(physics)
+	portions["physics"]=SeparateWhiteSpace(physics,"replace")#To denote replacement has to be done
 	portions["chemistry"]=SeparateWhiteSpace(chemistry)
 
 	#Just for testing, To be removed
@@ -78,9 +86,11 @@ if __name__=="__main__":
 			unitName = tempList[0]
 			if len(tempList)<2:#TEsting. Won't work for physics
 				continue 
-			splitTopic = tempList[1].split(',')	#The topics in the unit are split
+			topicList = re.sub('[\.;]',',',tempList[1])
+			splitTopic = topicList.split(',')	#The topics in the unit are split
 			for topic in splitTopic:
-				unit[topic] = unitName
+				if topic:
+					unit[topic] = unitName
 		finalPortions[subject+"_test"] = unit
 	WriteDictToFile(finalPortions)
 
